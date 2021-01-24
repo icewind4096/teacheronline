e8865ce2111f184c68c9aedd31ab890829b77e36
wangj <icewind4096@gmail.com>
2021-01-24 15:32:10 +0800

    统一结果返回，但是觉得实现的不好，后期用泛型类替代

--- service/service_edu/src/main/java/com/windvalley/guli/service/edu/controller/admin/TeacherController.java
+++ service/service_edu/src/main/java/com/windvalley/guli/service/edu/controller/admin/TeacherController.java
@@ -1,6 +1,7 @@
 package com.windvalley.guli.service.edu.controller.admin;
 
 
+import com.windvalley.guli.common.base.result.R;
 import com.windvalley.guli.service.edu.entity.Teacher;
 import com.windvalley.guli.service.edu.service.ITeacherService;
 import io.swagger.annotations.Api;
@@ -28,14 +29,20 @@ public class TeacherController {
 
     @ApiOperation("所有讲师列表")
     @PostMapping("list")
-    public List<Teacher> listAll(){
-        return teacherService.list();
+    public R listAll(){
+        List<Teacher> teacherList = teacherService.list();
+        return R.ok().data("items", teacherList);
     }
 
     @ApiOperation(value = "删除讲师", notes = "根据讲师ID,删除讲师")
     @PostMapping("remove/{id}")
-    public boolean removeById(@ApiParam("讲师ID") @PathVariable String id){
-        return teacherService.removeById(id);
+    public R removeById(@ApiParam("讲师ID") @PathVariable String id){
+        boolean result = teacherService.removeById(id);
+        if (result){
+            return R.ok().message("删除成功");
+        } else {
+            return R.error().message("数据不存在");
+        }
     }
 }
 
