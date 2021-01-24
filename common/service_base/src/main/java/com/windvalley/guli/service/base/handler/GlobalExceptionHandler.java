1ebc36805948f706d93d7087d68ab257e1ba9796
wangj <icewind4096@gmail.com>
2021-01-24 19:53:36 +0800

    日志配置以及错误日志输出

--- common/service_base/src/main/java/com/windvalley/guli/service/base/handler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/windvalley/guli/service/base/handler/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.windvalley.guli.service.base.handler;
 
 import com.windvalley.guli.common.base.result.R;
 import com.windvalley.guli.common.base.result.ResultCodeEnum;
+import com.windvalley.guli.common.base.util.ExceptionUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,20 +11,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 //控制器切面
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
     //需要精细捕获的异常，在此处添加
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public R error(Exception e){
-        e.printStackTrace();
+        log.error(ExceptionUtils.getMessage(e));
         return R.error();
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
     public R error(HttpMessageNotReadableException e){
-        e.printStackTrace();
+        log.error(ExceptionUtils.getMessage(e));
         return R.setResult(ResultCodeEnum.JSON_PARSE_ERROR);
     }
 }
